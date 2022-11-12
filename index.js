0a3a2657ac67917f548e787525d08da939e4c3c6
jp.8pedro86@gmail.com <jp.8pedro86@gmail.com>
2022-11-11 21:51:30 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Nome = prompt("Digite seu nome:");
 const Sobrenome = prompt("Digite seu sobrenome:");
 
-const NomeCompleto = confirm(`Seu nome completo é ${Nome} ${Sobrenome}\n Seu nome de catalogo é ${Sobrenome.toUpperCase()} ${Nome}`);
-const NomeCATOLOGO = confirm(`Seu nome de catalogo é ${Sobrenome.toUpperCase()} ${Nome}`);
+const NomeCompleto = confirm(`Seu nome completo é ${Nome} ${Sobrenome}\nSeu nome de catalogo é ${Sobrenome.toUpperCase()} ${Nome}`);
+
